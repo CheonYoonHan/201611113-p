a9bfc911986f673b82523feef7e6d70b0cd29795
yscheon <201611113@sangmyung.kr>
2016-03-25 00:55:44 +0900

    201611113 yscheon
    week3 assignment

---
+++ w3Main.py
@@ -0,0 +1,31 @@
+﻿#섭씨를 화씨로, 화씨를 섭씨로 변환하기
+## 섭씨를 화씨로, 화씨를 섭씨로 변환하기
+temp=input("User input temprtature : ")
+sel=raw_input("Fahrenheit or Celsius(F or C) : ")
+print "%.1f" %temp, sel
+if sel=="F":
+    result=(temp-32)/1.8
+    print "%.1f" %result,"C"
+elif sel=="f":
+    result=(temp-32)/1.8
+    print "%.1f" %result,"C"
+elif sel=="C":
+    result=temp*1.8+32
+    print "%.1f" %result,"F"
+elif sel=="c":
+    result=temp*1.8+32
+    print "%.1f" %result,"F"
+else:
+    print "input Error"
+
+me=raw_input("Correctly? (Yes or No) : ")
+if me=="Yes":
+    print "Thank you"
+elif me=="yes":
+    print "Thank you"
+elif me=="No":
+    print "Sorry"
+elif me=="no":
+    print "Sorry"
+
+
