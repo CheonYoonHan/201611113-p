de6b71705a16e883dbe8706388d4c0e94ab26769
yscheon <201611113@sangmyung.kr>
2016-04-03 17:05:14 +0900

    Added files via upload

---
+++ w5Main(M).py
@@ -0,0 +1,14 @@
+def kawibawibo():
+    'kawi'>'bo'
+    'bawi'>'kawi'
+    'bo'>'bawi'
+    userA=raw_input('userA input kawi, bawi, bo : ')
+    userB=raw_input('userB input kawi, bawi, bo : ')
+    if userA!=userB:
+        if userA>userB:
+            print 'UserA Win'
+        elif userA<userB:
+            print 'UserB Win'
+    else:
+        print 'draw'
+kawibawibo()
