817288139ccb3efe21a5aee7df34f13ada6ae111
201611113 천수빈 <201611113@sangmyung.kr>
2016-04-07 22:42:11 +0900

    Added files via upload

---
+++ w6Main11.py
@@ -0,0 +1,7 @@
+﻿#윤년 계산하기
+print "calculate Leap Year"
+year=input("User input year : ")
+if year==(year%4 == 0) and (year%100 !=0 or year%400==0):
+    print "User inputing year is Leap Year"
+else:
+    print "User inputing Year is Leap Year"
