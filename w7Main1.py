d6a68849a741876fba293b863d1ad7a8a0f98361
201611113 천수빈 <201611113@sangmyung.kr>
2016-04-11 17:18:25 +0900

    Added files via upload

---
+++ w7Main1.py
@@ -0,0 +1,28 @@
+﻿#리스트 함수 정의
+x=list()
+def l():
+	for i in range(0,1000):
+		if i % 4==0 and i%5!=0:
+			x.append(i)
+def main1():
+	l()
+	print x
+#리스트 합 구하는 함수 정의
+def sumlist(alist):
+	mysum=0
+	for i in range(0,len(alist)):
+		mysum=mysum+x[i]
+	return mysum
+def lab6():
+	l()
+	alist=x
+	labsum=sumlist(alist)
+	print labsum
+def main2():
+	lab6()
+#리스트 보이기
+main1()
+#리스트 합 구하기
+x=list()
+main2()
+input()
