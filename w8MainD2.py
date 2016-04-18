40db214ad5c1b3bc2f0acd302a7d0e540af8e9e0
201611113 천수빈 <201611113@sangmyung.kr>
2016-04-18 15:24:09 +0900

    데이터구조

---
+++ w8MainD2.py
@@ -0,0 +1,13 @@
+﻿#데이터구조-2: 거북이 트랙을 저장하기
+tracks=list()
+def drawSquareAt(size,pos):
+    t1.penup()
+    t1.setpos(pos)
+    t1.setheading(90)
+    t1.pendown()
+    for i in range(0,4):
+        tracks.append(t1.pos())
+        t1.right(90)
+        t1.fd(size)
+    print tracks
+drawSquareAt(100,(0,0))
