a1b8b7c5f13e81fdb6ff97bd944343e455faaab5
201611113 천수빈 <201611113@sangmyung.kr>
2016-05-15 22:56:06 +0900

    죄송합니다. 좀 늦었습니당

---
+++ w10main(speech).py
@@ -0,0 +1,139 @@
+﻿speech1=[
+    'Vice President Johnson, Mr. Speaker, Mr. Chief Justice, President Eisenhower, Vice President Nixon, President Truman, reverend clergy, fellow citizens, we observe today not a victory of party, but a celebration of freedom - symbolizing an end, as well as a beginning - signifying renewal, as well as change.'
+    ,'For I have sworn before you and Almighty God the same solemn oath our forebears prescribed nearly a century and three quarters ago.'
+    ,'The world is very different now.'
+    ,'For man holds in his mortal hands the power to abolish all forms of human poverty and all forms of human life.'
+    ,'And yet the same revolutionary beliefs for which our forebears fought are still at issue around the globe - the belief that the rights of man come not from the generosity of the state, but from the hand of God.'
+    ,'We dare not forget today that we are the heirs of that first revolution.'
+    ,'Let the word go forth from this time and place, to friend and foe alike, that the torch has been passed to a new generation of Americans - born in this century, tempered by war, disciplined by a hard and bitter peace, proud of our ancient heritage - and unwilling to witness or permit the slow undoing of those human rights to which this Nation has always been committed, and to which we are committed today at home and around the world.'
+    ,'Let every nation know, whether it wishes us well or ill, that we shall pay any price, bear any burden, meet any hardship, support any friend, oppose any foe, in order to assure the survival and the success of liberty.'
+    ,'This much we pledge - and more.'
+    ,'To those old allies whose cultural and spiritual origins we share, we pledge the loyalty of faithful friends. United, there is little we cannot do in a host of cooperative ventures.'
+    ,'Divided, there is little we can do - for we dare not meet a powerful challenge at odds and split asunder.'
+    ,'To those new States whom we welcome to the ranks of the free, we pledge our word that one form of colonial control shall not have passed away merely to be replaced by a far more iron tyranny. We shall not always expect to find them supporting our view.'
+    ,'But we shall always hope to find them strongly supporting their own freedom - and to remember that, in the past, those who foolishly sought power by riding the back of the tiger ended up inside.'
+    ,'To those peoples in the huts and villages across the globe struggling to break the bonds of mass misery, we pledge our best efforts to help them help themselves, for whatever period is required - not because the Communists may be doing it, not because we seek their votes, but because it is right.'
+    ,'If a free society cannot help the many who are poor, it cannot save the few who are rich.'
+    ,'To our sister republics south of our border, we offer a special pledge - to convert our good words into good deeds - in a new alliance for progress - to assist free men and free governments in casting off the chains of poverty.'
+    ,'But this peaceful revolution of hope cannot become the prey of hostile powers.'
+    ,'Let all our neighbors know that we shall join with them to oppose aggression or subversion anywhere in the Americas.'
+    ,'And let every other power know that this Hemisphere intends to remain the master of its own house.'
+    ,'To that world assembly of sovereign states, the United Nations, our last best hope in an age where the instruments of war have far outpaced the instruments of peace, we renew our pledge of support - to prevent it from becoming merely a forum for invective - to strengthen its shield of the new and the weak - and to enlarge the area in which its writ may run.'
+    ,'Finally, to those nations who would make themselves our adversary, we offer not a pledge but a request: that both sides begin anew the quest for peace, before the dark powers of destruction unleashed by science engulf all humanity in planned or accidental self-destruction.'
+    ,'We dare not tempt them with weakness.'
+    ,'For only when our arms are sufficient beyond doubt can we be certain beyond doubt that they will never be employed.'
+    ,'But neither can two great and powerful groups of nations take comfort from our present course - both sides overburdened by the cost of modern weapons, both rightly alarmed by the steady spread of the deadly atom, yet both racing to alter that uncertain balance of terror that stays the hand of mankinds final war.'
+    ,'So let us begin anew - remembering on both sides that civility is not a sign of weakness, and sincerity is always subject to proof. Let us never negotiate out of fear.'
+    ,'But let us never fear to negotiate.'
+    ,'Let both sides explore what problems unite us instead of belaboring those problems which divide us.'
+    ,'Let both sides, for the first time, formulate serious and precise proposals for the inspection and control of arms - and bring the absolute power to destroy other nations under the absolute control of all nations.'
+    ,'Let both sides seek to invoke the wonders of science instead of its terrors.'
+    ,'Together let us explore the stars, conquer the deserts, eradicate disease, tap the ocean depths, and encourage the arts and commerce.'
+    ,'Let both sides unite to heed in all corners of the earth the command of Isaiah - to "undo the heavy burdens ... and to let the oppressed go free."'
+    ,'And if a beachhead of cooperation may push back the jungle of suspicion, let both sides join in creating a new endeavor, not a new balance of power, but a new world of law, where the strong are just and the weak secure and the peace preserved.'
+    ,'All this will not be finished in the first 100 days.'
+    ,'Nor will it be finished in the first 1,000 days, nor in the life of this Administration, nor even perhaps in our l fetime on this planet.'
+    ,'But let us begin.'
+    ,'In your hands, my fellow citizens, more than in mine, will rest the final success or failure of our course.'
+    ,'Since this country was founded, each generation of Americans has been summoned to give testimony to its national loyalty.'
+    ,'The graves of young Americans who answered the call to service surround the globe.'
+    ,'Now the trumpet summons us again - not as a call to bear arms, though arms we need; not as a call to battle, though embattled we are - but a call to bear the burden of a long twilight struggle, year in and year out, "rejoicing in hope, patient in tribulation" - a struggle against the common enemies of man: tyranny, poverty, disease, and war itself.'
+    ,'Can we forge against these enemies a grand and global alliance, North and South, East and West, that can assure a more fruitful life for all mankind?'
+    ,'Will you join in that historic effort?'
+    ,'In the long history of the world, only a few generations have been granted the role of defending freedom in its hour of maximum danger.'
+    ,'I do not shrink from this responsibility - I welcome it.'
+    ,'I do not believe that any of us would exchange places with any other people or any other generation.'
+    ,'The energy, the faith, the devotion which we bring to this endeavor will light our country and all who serve it - and the glow from that fire can truly light the world.' 
+    ,'And so, my fellow Americans: ask not what your country can do for you - ask what you can do for your country.'
+    ,'My fellow citizens of the world: ask not what America will do for you, but what together we can do for the freedom of man.'
+    ,'Finally, whether you are citizens of America or citizens of the world, ask of us the same high standards of strength and sacrifice which we ask of you.'
+    ,'With a good conscience our only sure reward, with history the final judge of our deeds, let us go forth to lead the land we love, asking His blessing and His help, but knowing that here on earth Gods work must truly be our own.'
+    ]
+
+speech2=[
+    'On this day, prescribed by law and marked by ceremony, we celebrate the durable wisdom of our Constitution, and recall the deep commitments that unite our country.'
+    ,'I am grateful for the honor of this hour, mindful of the consequential times in which we live, and determined to fulfill the oath that I have sworn and you have witnessed.' 
+    ,'At this second gathering, our duties are defined not by the words I use, but by the history we have seen together.'
+    ,'For a half century, America defended our own freedom by standing watch on distant borders.'
+    ,'After the shipwreck of communism came years of relative quiet, years of repose, years of sabbatical —and then there came a day of fire.' 
+    ,'We have seen our vulnerability —and we have seen its deepest source. For as long as whole regions of the world simmer in resentment and tyranny —prone to ideologies that feed hatred and excuse murder — violence will gather, and multiply in destructive power, and cross the most defended borders, and raise a mortal threat.'
+    ,'There is only one force of history that can break the reign of hatred and resentment, and expose the pretensions of tyrants, and reward the hopes of the decent and tolerant, and that is the force of human freedom.'
+    ,'We are led, by events and common sense, to one conclusion:'
+    ,'The survival of liberty in our land increasingly depends on the success of liberty in other lands.'
+    ,'The best hope for peace in our world is the expansion of freedom in all the world.' 
+    ,'Americas vital interests and our deepest beliefs are now one.'
+    ,'From the day of our Founding, we have proclaimed that every man and woman on this earth has rights, and dignity, and matchless value, because they bear the image of the Maker of Heaven and earth. Across the generations we have proclaimed the imperative of self-government, because no one is fit to be a master, and no one deserves to be a slave.'
+    ,'Advancing these ideals is the mission that created our Nation.'
+    ,'It is the honorable achievement of our fathers.'
+    ,'Now it is the urgent requirement of our nations security, and the calling of our time.'
+    ,'So it is the policy of the United States to seek and support the growth of democratic movements and institutions in every nation and culture, with the ultimate goal of ending tyranny in our world.'
+    ,'This is not primarily the task of arms, though we will defend ourselves and our friends by force of arms when necessary.'
+    ,'Freedom, by its nature, must be chosen, and defended by citizens, and sustained by the rule of law and the protection of minorities.'
+    ,'And when the soul of a nation finally speaks, the institutions that arise may reflect customs and traditions very different from our own.'
+    ,'America will not impose our own style of government on the unwilling.'
+    ,'Our goal instead is to help others find their own voice, attain their own freedom, and make their own way.' 
+    ,'The great objective of ending tyranny is the concentrated work of generations.'
+    ,'The difficulty of the task is no excuse for avoiding it.'
+    ,'Americas influence is not unlimited, but fortunately for the oppressed, Americas influence is considerable, and we will use it confidently in freedoms cause.'
+    ,'My most solemn duty is to protect this nation and its people against further attacks and emerging threats.'
+    ,'Some have unwisely chosen to test Americas resolve, and have found it firm.'
+    ,'We will persistently clarify the choice before every ruler and every nation: The moral choice between oppression, which is always wrong, and freedom, which is eternally right. America will not pretend that jailed dissidents prefer their chains, or that women welcome humiliation and servitude,or that any human being aspires to live at the mercy of bullies.'
+    ,'We will encourage reform in other governments by making clear that success in our relations will require the decent treatment of their own people.'
+    ,"America's belief in human dignity will guide our policies, yet rights must be more than the grudging concessions of dictators; they are secured by free dissent and the participation of the governed."
+    ,"In the long run, there is no justice without freedom, and there can be no human rights without human liberty." 
+    ,"Some, I know, have questioned the global appeal of liberty —though this time in history, four decades defined by the swiftest advance of freedom ever seen, is an odd time for doubt. Americans, of all people, should never be surprised by the power of our ideals. Eventually, the call of freedom comes to every mind and every soul."
+    ," We do not accept the existence of permanent tyranny because we do not accept the possibility of permanent slavery." 
+    ,"Liberty will come to those who love it."
+    ,"Today, America speaks aneIn our time it means something still."
+    ,"America, in this young century, proclaims liberty throughout all the world, and to all the inhabitants thereof."
+    ,"Renewed in our strength — tested, but not weary — we are ready for the greatest achievements in the history of freedom."
+    ,"May God bless you, and may He watch over the United States of America." ]
+
+#단어 추출
+def countWords1(speech1):
+    a = {}
+    for sentence in speech1:
+        words=sentence.split()
+        for word in words:
+            if word in a:
+                a[word]+=1
+            else:
+                a[word]=1
+    return a
+
+def getWordsFreqGreater1(b,a):
+    c=list()
+    for k in a:
+        if a[k]>b:
+            c.append(k)
+    return c
+
+wordDict1=countWords1(speech1)
+freqWordsList1=getWordsFreqGreater1(2,wordDict1)
+
+#단어 추출
+def countWords2(speech2):
+    x = {}
+    for sentence in speech2:
+        words=sentence.split()
+        for word in words:
+            if word in x:
+                x[word]+=1
+            else:
+                x[word]=1
+    return x
+
+def getWordsFreqGreater2(y,x):
+    z=list()
+    for v in x:
+        if x[v]>y:
+            z.append(v)
+    return z
+wordDict2=countWords2(speech2)
+freqWordsList2=getWordsFreqGreater2(2,wordDict2)
+def compare():
+    compare1=set(freqWordsList1)
+    compare2=set(freqWordsList2)
+    return compare1&compare2
+def main():
+    compare()
